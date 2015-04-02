b9c49d58e6b48e6d41fece75099ff061d9e7a6ba
David Irvine <david.irvine@maidsafe.net>
2015-04-02 21:05:14 +0100

    Docs + gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! #crust
+//! Reliable p2p network connections in Rust with NAT traversal. 
+//! One of the most needed libraries for any server-less / decentralised projects
+
 #[test]
 fn it_works() {
 }
