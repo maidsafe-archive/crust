7bba038bb32b4effba184f8a101797ec62e6960c
David Irvine <david.irvine@maidsafe.net>
2015-04-07 11:28:50 +0100

    Added to crates.io just to hold place and name

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 //! Reliable p2p network connections in Rust with NAT traversal.
 //! One of the most needed libraries for any server-less / decentralised projects
 
+#![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
+       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+       html_root_url = "http:///dirvine.github.io/crust/crust/")]
 #![allow(unused_variables)]
 
 #![feature(alloc)]
