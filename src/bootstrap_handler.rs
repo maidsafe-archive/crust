6078fa74a9ea25a82bf072ea1d3743ee476bbbcf
BSMaidSafe <brian.smith@maidsafe.net>
2015-07-14 22:23:28 +0100

    Maintains a time ordered list of bootstrap contacts with max size 1500.

--- src/bootstrap_handler.rs
+++ src/bootstrap_handler.rs
@@ -24,13 +24,71 @@ use std::env;
 use rustc_serialize::json;
 use std::io;
 use itertools::Itertools;
+use rustc_serialize::{Decodable, Decoder, Encodable, Encoder};
 
+const MAX_CONTACTS: usize = 1500;
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+pub struct Timestamp {
+    pub timestamp: time::Tm,
+}
+
+impl Timestamp {
+
+    /// Construct a Timestamp with current UTC time.
+    pub fn new() -> Timestamp {
+        Timestamp { timestamp: time::now_utc() }
+    }
+}
+
+impl Encodable for Timestamp {
+    fn encode<E: Encoder>(&self, e: &mut E) -> Result<(), E::Error> {
+        e.emit_seq(11usize, |encoder| {
+            try!(encoder.emit_seq_elt(00usize, |encoder| self.timestamp.tm_sec.encode(encoder)));
+            try!(encoder.emit_seq_elt(01usize, |encoder| self.timestamp.tm_min.encode(encoder)));
+            try!(encoder.emit_seq_elt(02usize, |encoder| self.timestamp.tm_hour.encode(encoder)));
+            try!(encoder.emit_seq_elt(03usize, |encoder| self.timestamp.tm_mday.encode(encoder)));
+            try!(encoder.emit_seq_elt(04usize, |encoder| self.timestamp.tm_mon.encode(encoder)));
+            try!(encoder.emit_seq_elt(05usize, |encoder| self.timestamp.tm_year.encode(encoder)));
+            try!(encoder.emit_seq_elt(06usize, |encoder| self.timestamp.tm_wday.encode(encoder)));
+            try!(encoder.emit_seq_elt(07usize, |encoder| self.timestamp.tm_yday.encode(encoder)));
+            try!(encoder.emit_seq_elt(08usize, |encoder| self.timestamp.tm_isdst.encode(encoder)));
+            try!(encoder.emit_seq_elt(09usize, |encoder| self.timestamp.tm_utcoff.encode(encoder)));
+            try!(encoder.emit_seq_elt(10usize, |encoder| self.timestamp.tm_nsec.encode(encoder)));
+
+            Ok(())
+        })
+    }
+}
+
+impl Decodable for Timestamp {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Timestamp, D::Error> {
+        let (sec, min, hour, mday, mon, year, wday, yday, isdst, utcoff, nsec):
+            (i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, i32) = try!(Decodable::decode(d));
+
+        let timestamp = time::Tm {
+            tm_sec    : sec,
+            tm_min    : min,
+            tm_hour   : hour,
+            tm_mday   : mday,
+            tm_mon    : mon,
+            tm_year   : year,
+            tm_wday   : wday,
+            tm_yday   : yday,
+            tm_isdst  : isdst,
+            tm_utcoff : utcoff,
+            tm_nsec   : nsec
+        };
+
+        Ok(Timestamp { timestamp: timestamp })
+    }
+}
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone, RustcDecodable, RustcEncodable)]
 pub struct Contact {
     pub endpoint: Endpoint,
+    pub last_updated: Timestamp,
 }
-
 
 pub type Contacts = Vec<Contact>;
 
@@ -116,20 +174,40 @@ impl BootstrapHandler {
 
     fn insert_contacts(&mut self, contacts: Contacts) -> io::Result<()> {
         assert!(!contacts.is_empty());
-
-        let mut current_bootstrap = self.read_bootstrap_file()
+        let mut bootstrap = self.read_bootstrap_file()
             .unwrap_or_else(|e| {
                 println!("Failed to read Bootstrap file : {:?} ; {:?} ; Creating New file.",
                 self.file_name, e);
                 Bootstrap{ preferred_port: Port::Tcp(0u16),
-                                  hard_coded_contacts: Vec::new(),
-                                  contacts: Vec::new() }
+                           hard_coded_contacts: Vec::new(),
+                           contacts: Vec::new()
+                }
             });
 
+        let mut contact_list = contacts.clone();
+
         for contact in contacts {
-            current_bootstrap.contacts.push(contact.clone());
+            match bootstrap.contacts.binary_search_by(|e| e.endpoint.cmp(&contact.endpoint)) {
+                Ok(position) => { let _ = bootstrap.contacts.remove(position); },
+                Err(_) => {}
+            }
         }
-        self.write_bootstrap_file(current_bootstrap)
+
+        for contact in bootstrap.contacts {
+            contact_list.push(contact.clone());
+        }
+
+        let mut bootstrap_contacts = contact_list
+            .into_iter()
+            .sort_by(|a, b| Ord::cmp(&b.last_updated.timestamp, &a.last_updated.timestamp))
+            .into_iter()
+            .map(|contact| contact)
+            .collect::<Vec<Contact>>();
+
+        bootstrap_contacts.truncate(MAX_CONTACTS);
+        bootstrap.contacts = bootstrap_contacts;
+
+        self.write_bootstrap_file(bootstrap)
     }
 
     pub fn get_serialised_contacts(&self) -> io::Result<(Vec<u8>)> {
@@ -151,18 +229,37 @@ impl BootstrapHandler {
 #[cfg(test)]
     mod test {
     use super::*;
-    use std::net;
-    use std::net::SocketAddr;
+    use std::{net, fs};
+    use std::net::{SocketAddr, Ipv4Addr};
     use transport::{Endpoint, Port};
     use rustc_serialize::json;
-    use std::fs;
     use rand;
     use std::path::Path;
+    use time;
+    use itertools::Itertools;
+    use cbor::{Encoder, Decoder};
+    use rustc_serialize::{Decodable, Encodable};
+
+    use super::MAX_CONTACTS;
+
+    #[test]
+    fn timestamp() {
+        let time = time::now_utc();
+        let timestamp = Timestamp{ timestamp: time };
+        let mut timestamp_encoder = Encoder::from_memory();
+        timestamp_encoder.encode(&[&timestamp]).unwrap();
+        let mut timestamp_decoder = Decoder::from_bytes(timestamp_encoder.as_bytes());
+        let decoded_timestamp: Timestamp = timestamp_decoder.decode().next().unwrap().unwrap();
+        assert_eq!(timestamp, decoded_timestamp);
+    }
 
     #[test]
     fn serialisation() {
         let addr = net::SocketAddrV4::new(net::Ipv4Addr::new(1,2,3,4), 8080);
-        let contact  = Contact { endpoint: Endpoint::Tcp(SocketAddr::V4(addr)) };
+        let contact  = Contact {
+            endpoint: Endpoint::Tcp(SocketAddr::V4(addr)),
+            last_updated: Timestamp{ timestamp: time::empty_tm() }
+        };
         let mut contacts = Contacts::new();
         contacts.push(contact.clone());
         contacts.push(contact.clone());
@@ -187,8 +284,11 @@ impl BootstrapHandler {
             let port_0: u16 = rand::random::<u16>();
             let addr_0 = net::SocketAddrV4::new(net::Ipv4Addr::new(random_addr_0[0],
                 random_addr_0[1], random_addr_0[2], random_addr_0[3]), port_0);
-            let new_contact = Contact{ endpoint: Endpoint::Tcp(SocketAddr::V4(addr_0)) };
-                contacts.push(new_contact);
+            let new_contact = Contact{
+                endpoint: Endpoint::Tcp(SocketAddr::V4(addr_0)),
+                last_updated: Timestamp{ timestamp: time::now_utc() }
+            };
+            contacts.push(new_contact);
         }
 
         let file_name = BootstrapHandler::get_file_name();
@@ -210,6 +310,81 @@ impl BootstrapHandler {
 
         assert_eq!(read_contacts, contacts);
 
+        match fs::remove_file(file_name.clone()) {
+            Ok(_) => (),
+            Err(e) => println!("Failed to remove {}: {}", file_name, e),
+        };
+    }
+
+    #[test]
+    fn bootstrap_handler_max_contacts() {
+        let mut contacts = Vec::new();
+        for _ in 0..MAX_CONTACTS {
+            let mut ip = Vec::with_capacity(4);
+
+            ip.push(rand::random::<u8>());
+            ip.push(rand::random::<u8>());
+            ip.push(rand::random::<u8>());
+            ip.push(rand::random::<u8>());
+
+            let port = rand::random::<u16>();
+            let ipport = net::SocketAddrV4::new(Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]), port);
+            let contact = Contact{
+                endpoint: Endpoint::Tcp(SocketAddr::V4(ipport)),
+                last_updated: Timestamp{ timestamp: time::now_utc() }
+            };
+            contacts.push(contact);
+        }
+
+        let file_name = BootstrapHandler::get_file_name();
+        let path = Path::new(&file_name);
+        let mut bootstrap_handler = BootstrapHandler::new();
+        let file = fs::File::create(&path);
+
+        // check that the file got created...
+        assert!(file.is_ok());
+
+        // insert contacts...
+        assert!(bootstrap_handler.insert_contacts(contacts.clone()).is_ok());
+        let bootstrap: Bootstrap = bootstrap_handler.read_bootstrap_file().unwrap();
+        let recovered_contacts : Contacts = bootstrap.contacts;
+        // sort contacts w.r.t increasing last updated times...
+        let sorted_contacts = contacts
+            .into_iter()
+            .sort_by(|a, b| Ord::cmp(&b.last_updated.timestamp, &a.last_updated.timestamp))
+            .into_iter()
+            .map(|contact| contact)
+            .collect::<Vec<Contact>>();
+
+        // check that the recovered contacts are the same as the originals...
+        assert_eq!(recovered_contacts, sorted_contacts);
+        // check the number of contacts is MAX_CONTACTS...
+        assert_eq!(recovered_contacts.len(), MAX_CONTACTS);
+
+        // get the last contact in the list and update it...
+        let mut updated_contact = recovered_contacts[recovered_contacts.len() - 1].clone();
+        let mut updated_contacts = Vec::new();
+
+        updated_contact.last_updated.timestamp = time::now_utc();
+        updated_contacts.push(updated_contact.clone());
+
+        // insert the updated contact...
+        assert!(bootstrap_handler.insert_contacts(updated_contacts.clone()).is_ok());
+        let bootstrap: Bootstrap = bootstrap_handler.read_bootstrap_file().unwrap();
+        let recovered_contacts : Contacts = bootstrap.contacts;
+
+        // check that the recovered contacts are not the same as the originals...
+        assert!(recovered_contacts != sorted_contacts);
+        // check the number of contacts is still MAX_CONTACTS...
+        assert_eq!(recovered_contacts.len(), MAX_CONTACTS);
+        // check that the updated contact is not still at the end of the list...
+        let last_contact = recovered_contacts[recovered_contacts.len() - 1].clone();
+        assert!(last_contact != updated_contact.clone());
+        // check that the updated contact is at the start of the list...
+        let first_contact = recovered_contacts[0].clone();
+        assert_eq!(first_contact, updated_contact.clone());
+
+        // remove the bootstrap file from disk...
         match fs::remove_file(file_name.clone()) {
             Ok(_) => (),
             Err(e) => println!("Failed to remove {}: {}", file_name, e),
