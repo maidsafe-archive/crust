bf12d5f9f669027f4cdc4de333d980a79fdf58aa
maqi <qi.ma@maidsafe.net>
2015-10-08 07:27:28 +0100

    move bad_style from forbid to deny

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,9 @@
 
 // For explanation of lint checks, run `rustc -W help` or see
 // https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
-#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+#![forbid(exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
           unknown_crate_types, warnings)]
-#![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
+#![deny(bad_style, deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
         non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
         private_no_mangle_fns, private_no_mangle_statics, raw_pointer_derive, stable_features,
         unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
