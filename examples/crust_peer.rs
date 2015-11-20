565d2c7dac2d12aacba5f3f14457e8790a36cc8d
Vinícius dos Santos Oliveira <vinipsmaker@maidsafe.net>
2015-11-20 01:37:12 -0300

    remove `raw_pointer_derive` linter
    This lint was removed from Rust recently and is causing builds to fail:
    https://github.com/rust-lang/rust/pull/29882
    
    Fixes #427.

--- examples/crust_peer.rs
+++ examples/crust_peer.rs
@@ -20,7 +20,7 @@
 #![forbid(missing_docs, warnings)]
 #![deny(bad_style, deprecated, drop_with_repr_extern, improper_ctypes, non_shorthand_field_patterns,
         overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
-        raw_pointer_derive, stable_features, unconditional_recursion, unknown_lints,
+        stable_features, unconditional_recursion, unknown_lints,
         unsafe_code, unused_allocation, unused_attributes,
         unused_comparisons, unused_features, unused_parens, while_true)]
 #![warn(trivial_casts, trivial_numeric_casts, unused, unused_extern_crates, unused_import_braces,
--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@
           unknown_crate_types, warnings)]
 #![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
         non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
-        private_no_mangle_fns, private_no_mangle_statics, raw_pointer_derive, stable_features,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features,
         unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
         unused_attributes, unused_comparisons, unused_features, unused_parens, while_true)]
 #![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
