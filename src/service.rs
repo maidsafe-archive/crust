fb5c1920c78b91da5cd351c6b2d65779639d93b0
maqi <qi.ma@maidsafe.net>
2015-12-01 06:47:13 +0000

    network_tcp test does trow lost_connection event

--- src/service.rs
+++ src/service.rs
@@ -971,8 +971,7 @@ mod test {
                                         }
                                     }
                                     Event::LostConnection(_) => {
-                                        println!("connection lost");
-                                        break;
+                                        debug!("connection lost");
                                     }
                                     _ => {
                                         println!("Received unknown event {:?}", event);
