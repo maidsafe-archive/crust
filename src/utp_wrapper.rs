b40e6ab489bd64e05cdb1705ea4c4b9cde6ad856
maqi <qi.ma@maidsafe.net>
2015-12-10 06:11:13 +0000

    increase attempts little bit

--- src/utp_wrapper.rs
+++ src/utp_wrapper.rs
@@ -50,7 +50,7 @@ impl UtpWrapper {
                                 Err(TryRecvError::Disconnected) => break 'outer,
                                 Err(TryRecvError::Empty) => {
                                     attempts += 1;
-                                    if attempts < 10 {
+                                    if attempts < 50 {
                                         break;
                                     } else {
                                         break 'outer;
