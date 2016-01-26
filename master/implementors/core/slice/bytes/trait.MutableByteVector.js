4da789280a3dd8f5377071a3f34888a056193fef
MaidSafe-QA <qa@maidsafe.net>
2016-01-26 14:26:17 +0000

    Updated documentation.

--- master/implementors/core/slice/bytes/trait.MutableByteVector.js
+++ master/implementors/core/slice/bytes/trait.MutableByteVector.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['hyper'] = [];implementors['igd'] = [];implementors['crust'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['hyper'] = [];implementors['igd'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
