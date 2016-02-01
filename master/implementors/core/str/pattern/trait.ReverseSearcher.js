a5613a00b053e8247282414a892fac28ad7ed1cd
MaidSafe-QA <qa@maidsafe.net>
2016-02-01 08:09:23 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.ReverseSearcher.js
+++ master/implementors/core/str/pattern/trait.ReverseSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['mio'] = [];implementors['crust'] = [];
+implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['mio'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
