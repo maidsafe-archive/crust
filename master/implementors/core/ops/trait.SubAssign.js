db6ffd9b432befa6f1d7ace066797d75f42436da
MaidSafe-QA <qa@maidsafe.net>
2016-02-03 05:08:03 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.SubAssign.js
+++ master/implementors/core/ops/trait.SubAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['mio'] = [];implementors['crust'] = [];
+implementors['libc'] = [];implementors['mio'] = [];implementors['socket_addr'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
