460b0be3b68added672baf1b383950cdac3a64e2
Vinícius dos Santos Oliveira <vinipsmaker@maidsafe.net>
2016-03-01 08:36:19 -0300

    Revert "disable uTP support"
    This reverts commit 4b84f68884be348e715d7d1910ac30f155c4fb4a.

--- src/connection.rs
+++ src/connection.rs
@@ -128,7 +128,7 @@ pub fn connect(peer_contact: StaticContactInfo,
                connection_map: Arc<Mutex<HashMap<PeerId, Vec<Connection>>>>,
                mc: &MappingContext)
                -> io::Result<()> {
-    let mut last_err = io::Error::new(io::ErrorKind::NotFound, "No TCP acceptors found.");
+    let mut last_err = io::Error::new(io::ErrorKind::NotFound, "No TCP/uTP acceptors found.");
     for tcp_addr in peer_contact.tcp_acceptors {
         match connect_tcp_endpoint(tcp_addr,
                                    our_contact_info.clone(),
@@ -141,7 +141,6 @@ pub fn connect(peer_contact: StaticContactInfo,
         }
     }
 
-/*
     let (udp_socket, (our_priv_info, our_pub_info)) = {
         match MappedUdpSocket::new(mc).result_discard() {
             Ok(MappedUdpSocket { socket, endpoints }) => {
@@ -204,7 +203,6 @@ pub fn connect(peer_contact: StaticContactInfo,
             },
         }
     }
-*/
 
     Err(last_err)
 }
