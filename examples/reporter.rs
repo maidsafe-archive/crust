cc994151793c6c743459d6e90a3b498f2a266ef3
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-03-03 00:47:08 +0000

    Updated dependencies and disabled Clippy `use_debug` check.

--- examples/reporter.rs
+++ examples/reporter.rs
@@ -34,6 +34,7 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 #![cfg_attr(feature="clippy", deny(clippy, clippy_pedantic))]
+#![cfg_attr(feature="clippy", allow(use_debug))]
 
 #[macro_use]
 extern crate maidsafe_utilities;
