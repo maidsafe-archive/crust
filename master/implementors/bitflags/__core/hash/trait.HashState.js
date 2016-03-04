710f7e0eeed964735dbd081ea324a36ed0346b37
MaidSafe-QA <qa@maidsafe.net>
2016-03-04 13:29:46 +0000

    Updated documentation.

---
+++ master/implementors/bitflags/__core/hash/trait.HashState.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bitflags'] = ["impl&lt;H&gt; <a class='trait' href='bitflags/__core/hash/trait.HashState.html' title='bitflags::__core::hash::HashState'>HashState</a> for <a class='struct' href='bitflags/__core/collections/hash_state/struct.DefaultState.html' title='bitflags::__core::collections::hash_state::DefaultState'>DefaultState</a>&lt;H&gt; <span class='where'>where H: <a class='trait' href='bitflags/__core/default/trait.Default.html' title='bitflags::__core::default::Default'>Default</a> + <a class='trait' href='bitflags/__core/hash/trait.Hasher.html' title='bitflags::__core::hash::Hasher'>Hasher</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
