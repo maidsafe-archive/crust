2fd43c03c2b18db5b4fe3d9b72d45082e5dec00b
MaidSafe-QA <qa@maidsafe.net>
2016-03-15 07:33:48 +0000

    Updated documentation.

--- master/regex/literals/sidebar-items.js
+++ master/regex/literals/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["BuildPrefixes",""],["Literals","A prefix extracted from a compiled regular expression."]]});
+initSidebarItems({"struct":[["Literals","A prefix extracted from a compiled regular expression."]]});
