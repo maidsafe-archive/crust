fe55354dcbe607568f04a32706753f4370899c6d
MaidSafe-QA <qa@maidsafe.net>
2016-03-18 13:19:40 +0000

    Updated documentation.

--- master/implementors/core/num/dec2flt/rawfp/trait.RawFloat.js
+++ master/implementors/core/num/dec2flt/rawfp/trait.RawFloat.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['libc'] = [];implementors['hyper'] = [];implementors['igd'] = [];
+implementors['libc'] = [];implementors['bitflags'] = [];implementors['hyper'] = [];implementors['igd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
