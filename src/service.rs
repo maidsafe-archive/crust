2eebf99a3841aba95175d6a15c06f012477ea69d
Vinícius dos Santos Oliveira <vinipsmaker@maidsafe.net>
2016-03-21 05:02:01 -0300

    more info on failure to create UDP hole punch server

--- src/service.rs
+++ src/service.rs
@@ -200,10 +200,10 @@ impl Service {
                                            config.enable_utp);
 
         let udp_hole_punch_server = try!(SimpleUdpHolePunchServer::new(mapping_context.clone())
-                                             .result_log()
-                                             .or(Err(io::Error::new(io::ErrorKind::Other,
-                                                                    "Failed to create UDP \
-                                                                     hole punch server"))));
+                                             .result_log().or_else(|err| {
+                                                 let e: io::Error = From::from(err);
+                                                 Err(e)
+                                             }));
         let tcp_hole_punch_server = try!(SimpleTcpHolePunchServer::new(mapping_context.clone())
                                              .result_log()
                                              .or(Err(io::Error::new(io::ErrorKind::Other,
