6337337d8ccc87984e3e5c101224f4240ea9d0ac
MaidSafe-QA <qa@maidsafe.net>
2016-05-12 13:11:10 +0000

    Updated documentation.

--- master/implementors/std/io/trait.BufRead.js
+++ master/implementors/std/io/trait.BufRead.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bufstream'] = ["impl&lt;S: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for <a class='struct' href='bufstream/struct.BufStream.html' title='bufstream::BufStream'>BufStream</a>&lt;S&gt;",];implementors['mio'] = [];implementors['socket_addr'] = [];implementors['crust'] = [];
+implementors['bufstream'] = ["impl&lt;S: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for <a class='struct' href='bufstream/struct.BufStream.html' title='bufstream::BufStream'>BufStream</a>&lt;S&gt;",];implementors['socket_addr'] = [];implementors['mio'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
