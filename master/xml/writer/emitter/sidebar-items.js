a848e509c18af1e85ae4b4809f0907eb03a15760
MaidSafe-QA <qa@maidsafe.net>
2016-05-12 15:19:35 +0000

    Updated documentation.

--- master/xml/writer/emitter/sidebar-items.js
+++ master/xml/writer/emitter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"type":[["EmitterResult",""]]});
+initSidebarItems({"enum":[["EmitterError","An error which may be returned by `XmlWriter` when writing XML events."]],"type":[["Result","A result type yielded by `XmlWriter`."]]});
