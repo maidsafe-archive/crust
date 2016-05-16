9d0b9fad9c1ab6b266061a1ce92c57310e9341d9
maqi <qi.ma@maidsafe.net>
2016-05-16 14:19:16 +0100

    feat/active_connections: Send data_len first then data
    Sending data_len first then data.
    On receiving side, read size first, then reading data

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,7 @@
 #![cfg_attr(feature="clippy", deny(clippy, clippy_pedantic))]
 #![cfg_attr(feature="clippy", allow(use_debug))]
 
+extern crate byteorder;
 #[macro_use]
 extern crate maidsafe_utilities;
 extern crate mio;
