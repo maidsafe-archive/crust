9aae26d6c6dc6b8c614a1e5ee01ec4db2d4a5a57
Adam Cigánek <adam.ciganek@gmail.com>
2016-05-26 15:02:08 +0200

    Test reorganization

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,10 @@ extern crate crossbeam;
 #[cfg(test)]
 extern crate void;
 
+#[cfg(test)]
+#[macro_use]
+mod tests;
+
 mod connect;
 mod active_connection;
 mod bootstrap_states;
@@ -82,8 +86,6 @@ mod socket;
 mod static_contact_info;
 pub mod nat;
 
-#[cfg(test)]
-mod tests;
 
 /// Crust Observers will be informed of crust events on this
 pub type CrustEventSender = ::maidsafe_utilities::event_sender::MaidSafeObserver<Event>;
