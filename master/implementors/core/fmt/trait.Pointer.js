bdbc3533a29fc9bcd6a76aad4b46326dc9cabfb5
MaidSafe-QA <qa@maidsafe.net>
2016-06-20 18:10:52 +0000

    Updated documentation.

--- master/implementors/core/fmt/trait.Pointer.js
+++ master/implementors/core/fmt/trait.Pointer.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['serde'] = [];implementors['hyper'] = [];implementors['igd'] = [];
+implementors['serde'] = [];implementors['libc'] = [];implementors['hyper'] = [];implementors['igd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
