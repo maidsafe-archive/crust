4cd7d4a0f9d8c131fab5683339173ea813f2d7d1
MaidSafe-QA <qa@maidsafe.net>
2016-06-24 11:47:02 +0000

    Updated documentation.

--- master/implementors/std/ascii/trait.AsciiExt.js
+++ master/implementors/std/ascii/trait.AsciiExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['crust'] = [];
+implementors['tmp_mio'] = [];implementors['mio'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
