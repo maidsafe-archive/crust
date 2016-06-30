9dd18ca1710d908df27882aef6928b2005b01347
MaidSafe-QA <qa@maidsafe.net>
2016-06-30 15:20:21 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Pattern.js
+++ master/implementors/core/str/pattern/trait.Pattern.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['tmp_mio'] = [];implementors['mio'] = [];implementors['serde'] = [];implementors['hyper'] = [];implementors['maidsafe_utilities'] = [];implementors['igd'] = [];implementors['crust'] = [];
+implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['serde'] = [];implementors['hyper'] = [];implementors['maidsafe_utilities'] = [];implementors['igd'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
