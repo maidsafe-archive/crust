bb8a958ac4295945f91ba9d7389122c4091df0d0
Povilas Balciunas <balciunas90@gmail.com>
2018-01-08 13:37:54 +0200

    chore/lints: check for unused results

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@
         unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
         unused_attributes, unused_comparisons, unused_features, unused_parens, while_true)]
 #![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
-        unused_qualifications)]
+        unused_qualifications, unused_results)]
 #![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
          missing_debug_implementations, variant_size_differences)]
 
