2aeb833769a836d34c72a96c1d70377645d8d497
Povilas Balciunas <balciunas90@gmail.com>
2018-03-01 17:20:09 +0200

    feat/socket: add method to get inner stream
    Socket wraps FramedPaStream. There was a need to get the inner stream so that we could send packets bigger than our limit.
    Also SocketTask was made generic so that we could test it without constructing PaStream.

--- src/net/socket.rs
+++ src/net/socket.rs
@@ -18,6 +18,7 @@
 use bytes::BytesMut;
 use futures::stream::{SplitSink, SplitStream};
 use futures::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use futures::sync::oneshot;
 use log::LogLevel;
 use priv_prelude::*;
 use tokio_io;
@@ -83,17 +84,27 @@ pub struct Inner {
     crypto_ctx: CryptoContext,
 }
 
-enum TaskMsg {
+enum TaskMsg<T = FramedPaStream>
+where
+    T: Stream<Item = BytesMut>,
+    T: Sink<SinkItem = BytesMut, SinkError = io::Error>,
+{
     Send(Priority, BytesMut),
-    Shutdown(SplitStream<FramedPaStream>),
+    Shutdown(SplitStream<T>),
+    /// Tells `SocketTask` to close it's activity, reunite and return the inner stream.
+    GetInnerStream(oneshot::Sender<T>, SplitStream<T>),
 }
 
-struct SocketTask {
+struct SocketTask<T = FramedPaStream>
+where
+    T: Stream<Item = BytesMut>,
+    T: Sink<SinkItem = BytesMut, SinkError = io::Error>,
+{
     handle: Handle,
-    stream_rx: Option<SplitStream<FramedPaStream>>,
-    stream_tx: Option<SplitSink<FramedPaStream>>,
+    stream_rx: Option<SplitStream<T>>,
+    stream_tx: Option<SplitSink<T>>,
     write_queue: BTreeMap<Priority, VecDeque<(Instant, BytesMut)>>,
-    write_rx: UnboundedReceiver<TaskMsg>,
+    write_rx: UnboundedReceiver<TaskMsg<T>>,
 }
 
 impl<M: 'static> Socket<M> {
@@ -155,12 +166,29 @@ impl<M: 'static> Socket<M> {
             _ph: PhantomData,
         }
     }
+
+    /// Returns an inner stream that is wrapped by this `Socket`.
+    /// This method is only meant to be used in tests.
+    /// Note, that socket write buffer will be destroyed.
+    pub fn into_inner(mut self) -> BoxFuture<FramedPaStream, SocketError> {
+        let mut inner = try_bfut!(self.inner.take().ok_or(SocketError::Destroyed));
+        let stream_rx = unwrap!(inner.stream_rx.take());
+        let (inner_stream_tx, inner_stream_rx) = oneshot::channel();
+        let _ = inner.write_tx.unbounded_send(TaskMsg::GetInnerStream(
+            inner_stream_tx,
+            stream_rx,
+        ));
+        inner_stream_rx
+            .map_err(|_e| SocketError::Destroyed)
+            .into_boxed()
+    }
 }
 
 impl Drop for Inner {
     fn drop(&mut self) {
-        let stream_rx = unwrap!(self.stream_rx.take());
-        let _ = self.write_tx.unbounded_send(TaskMsg::Shutdown(stream_rx));
+        if let Some(stream_rx) = self.stream_rx.take() {
+            let _ = self.write_tx.unbounded_send(TaskMsg::Shutdown(stream_rx));
+        }
     }
 }
 
@@ -219,9 +247,14 @@ where
     }
 }
 
-impl SocketTask {
+impl<T> SocketTask<T>
+where
+    T: Stream<Item = BytesMut>,
+    T: Sink<SinkItem = BytesMut, SinkError = io::Error>,
+{
     /// Check if there's anything to send. If there is, enqueue the messages.
-    fn poll_task(&mut self) {
+    /// Returns true, when socket task should be terminated, false otherwise.
+    fn poll_task(&mut self) -> bool {
         let now = Instant::now();
         loop {
             match unwrap!(self.write_rx.poll()) {
@@ -235,19 +268,30 @@ impl SocketTask {
                     self.stream_rx = Some(stream_rx);
                     break;
                 }
+                Async::Ready(Some(TaskMsg::GetInnerStream(send_me_stream, stream_rx))) => {
+                    let stream_tx = unwrap!(self.stream_tx.take());
+                    let inner_stream = unwrap!(stream_rx.reunite(stream_tx));
+                    let _ = send_me_stream.send(inner_stream);
+                    return true;
+                }
                 Async::Ready(None) |
                 Async::NotReady => break,
             }
         }
+        false
     }
 }
 
-impl Future for SocketTask {
+impl Future for SocketTask<FramedPaStream> {
     type Item = ();
     type Error = io::Error;
 
     fn poll(&mut self) -> io::Result<Async<()>> {
-        self.poll_task();
+        let close_socket_task = self.poll_task();
+        if close_socket_task {
+            return Ok(Async::Ready(()));
+        }
+
         let expired_keys: Vec<u8> = self.write_queue
             .iter()
             .skip_while(|&(&priority, queue)| {
@@ -315,83 +359,134 @@ impl Future for SocketTask {
 mod test {
     use super::*;
 
-    use rand::{self, Rng};
     use rust_sodium::crypto::box_::gen_keypair;
     use tokio_core::reactor::Core;
 
     use util;
 
-    fn random_msgs(num_msgs: usize) -> Vec<Vec<u8>> {
-        let mut msgs = Vec::with_capacity(num_msgs);
-        for _ in 0..num_msgs {
-            let size = rand::thread_rng().gen_range(0, 10_000);
-            let data = util::random_vec(size);
-            let msg = data;
-            msgs.push(msg);
+    mod socket {
+        use super::*;
+        use rand::{self, Rng};
+
+        fn random_msgs(num_msgs: usize) -> Vec<Vec<u8>> {
+            let mut msgs = Vec::with_capacity(num_msgs);
+            for _ in 0..num_msgs {
+                let size = rand::thread_rng().gen_range(0, 10_000);
+                let data = util::random_vec(size);
+                let msg = data;
+                msgs.push(msg);
+            }
+            msgs
         }
-        msgs
+
+        fn exchange_messages(bind_addr: PaAddr) {
+            let mut core = unwrap!(Core::new());
+            let handle = core.handle();
+            let config = unwrap!(ConfigFile::new_temporary());
+            let (listener_pk, listener_sk) = gen_keypair();
+            let anon_decrypt_ctx =
+                CryptoContext::anonymous_decrypt(listener_pk, listener_sk.clone());
+
+            let listener = unwrap!(PaListener::bind(
+                &bind_addr,
+                &handle,
+                anon_decrypt_ctx.clone(),
+                listener_sk,
+            ));
+            let addr = unwrap!(listener.local_addr()).unspecified_to_localhost();
+
+            let num_msgs = 1000;
+            let msgs = random_msgs(num_msgs);
+
+            let f0 = {
+                let msgs: Vec<(Priority, _)> = msgs.iter().cloned().map(|m| (1, m)).collect();
+                let handle = handle.clone();
+                PaStream::direct_connect(&handle, &addr, listener_pk, &config)
+                    .map_err(SocketError::from)
+                    .and_then(move |(stream, _peer_addr)| {
+                        let socket = Socket::<Vec<u8>>::wrap_pa(
+                            &handle,
+                            stream,
+                            addr,
+                            CryptoContext::null(),
+                        );
+                        socket
+                            .send_all(stream::iter_ok::<_, SocketError>(msgs))
+                            .map(|(_, _)| ())
+                    })
+            };
+
+            let f1 = {
+                let handle = handle.clone();
+                listener
+                    .incoming()
+                    .into_future()
+                    .map_err(|(err, _)| panic!("incoming error: {}", err))
+                    .and_then(move |(stream_opt, _)| {
+                        let (stream, addr) = unwrap!(stream_opt);
+                        let socket = Socket::<Vec<u8>>::wrap_pa(
+                            &handle,
+                            stream,
+                            addr,
+                            CryptoContext::null(),
+                        );
+                        socket.take(num_msgs as u64).collect().map(
+                            move |msgs_recv| {
+                                assert_eq!(msgs_recv, msgs);
+                            },
+                        )
+                    })
+            };
+
+            let _ = unwrap!(core.run(f0.join(f1)));
+        }
+
+        #[test]
+        fn it_exchanges_messages_via_tcp() {
+            exchange_messages(tcp_addr!("0.0.0.0:0"));
+        }
+
+        #[test]
+        fn it_exchanges_messages_via_utp() {
+            exchange_messages(utp_addr!("0.0.0.0:0"));
+        }
     }
 
-    fn exchange_messages(bind_addr: PaAddr) {
-        let mut core = unwrap!(Core::new());
-        let handle = core.handle();
-        let config = unwrap!(ConfigFile::new_temporary());
-        let (listener_pk, listener_sk) = gen_keypair();
-        let anon_decrypt_ctx = CryptoContext::anonymous_decrypt(listener_pk, listener_sk.clone());
+    mod socket_task {
+        use super::*;
+        use future_utils::bi_channel;
 
-        let listener = unwrap!(PaListener::bind(
-            &bind_addr,
-            &handle,
-            anon_decrypt_ctx.clone(),
-            listener_sk,
-        ));
-        let addr = unwrap!(listener.local_addr()).unspecified_to_localhost();
+        mod poll_task {
+            use super::*;
 
-        let num_msgs = 1000;
-        let msgs = random_msgs(num_msgs);
+            #[test]
+            fn when_task_is_get_inner_stream_it_returns_true() {
+                let mut evloop = unwrap!(Core::new());
+                let handle = evloop.handle();
 
-        let f0 = {
-            let msgs: Vec<(Priority, _)> = msgs.iter().cloned().map(|m| (1, m)).collect();
-            let handle = handle.clone();
-            PaStream::direct_connect(&handle, &addr, listener_pk, &config)
-                .map_err(SocketError::from)
-                .and_then(move |(stream, _peer_addr)| {
-                    let socket =
-                        Socket::<Vec<u8>>::wrap_pa(&handle, stream, addr, CryptoContext::null());
-                    socket
-                        .send_all(stream::iter_ok::<_, SocketError>(msgs))
-                        .map(|(_, _)| ())
-                })
-        };
+                let (channel, _other_channel) = bi_channel::unbounded();
+                let channel = channel.sink_map_err(|_e| {
+                    io::Error::new(io::ErrorKind::Other, "sink.send() failed")
+                });
+                let (stream_tx, stream_rx) = channel.split();
 
-        let f1 = {
-            let handle = handle.clone();
-            listener
-                .incoming()
-                .into_future()
-                .map_err(|(err, _)| panic!("incoming error: {}", err))
-                .and_then(move |(stream_opt, _)| {
-                    let (stream, addr) = unwrap!(stream_opt);
-                    let socket =
-                        Socket::<Vec<u8>>::wrap_pa(&handle, stream, addr, CryptoContext::null());
-                    socket.take(num_msgs as u64).collect().map(
-                        move |msgs_recv| {
-                            assert_eq!(msgs_recv, msgs);
-                        },
-                    )
-                })
-        };
+                let (task_tx, task_rx) = mpsc::unbounded();
+                let mut task = SocketTask {
+                    handle: handle.clone(),
+                    stream_tx: Some(stream_tx),
+                    stream_rx: None,
+                    write_queue: BTreeMap::new(),
+                    write_rx: task_rx,
+                };
 
-        let _ = unwrap!(core.run(f0.join(f1)));
-    }
+                let (inner_stream_tx, _inner_stream_rx) = oneshot::channel();
+                let send_task = task_tx.send(TaskMsg::GetInnerStream(inner_stream_tx, stream_rx));
+                let _ = unwrap!(evloop.run(send_task));
 
-    #[test]
-    fn socket_exchanges_messages_via_tcp() {
-        exchange_messages(tcp_addr!("0.0.0.0:0"));
-    }
+                let close_socket_task = task.poll_task();
 
-    #[test]
-    fn socket_exchanges_messages_via_utp() {
-        exchange_messages(utp_addr!("0.0.0.0:0"));
+                assert!(close_socket_task);
+            }
+        }
     }
 }
